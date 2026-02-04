ba003f273c9bd323d9668bfd32aa93b4e4c4cd5d
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 14:17:38 +0500

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { db } from "../db.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await db.collection("products").find().toArray();
+  res.json(products);
+});
+
+export default router;
