6e13689993c1697ec5d3988fcf694cab07fe4642
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 13:12:36 +0500

    Update aggregation.js

--- queries/aggregation.js
+++ queries/aggregation.js
@@ -1,1 +1,44 @@
+db.orders.aggregate([
+  {
+    $group: {
+      _id: "$user_id",
+      totalOrders: { $sum: 1 }
+    }
+  },
+  {
+    $sort: { totalOrders: -1 }
+  }
+])
 
+db.orders.aggregate([
+  {
+    $group: {
+      _id: "$user_id",
+      totalSpent: { $sum: "$total_price" }
+    }
+  },
+  {
+    $sort: { totalSpent: -1 }
+  }
+])
+
+db.reviews.aggregate([
+  {
+    $group: {
+      _id: "$product_id",
+      averageRating: { $avg: "$rating" },
+      reviewCount: { $sum: 1 }
+    }
+  }
+])
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "users",
+      localField: "user_id",
+      foreignField: "_id",
+      as: "user_info"
+    }
+  }
+])
