221b455101e40b8cd4d6d6db866eefe724c5abdb
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 13:13:58 +0500

    Update indexes.js

--- queries/indexes.js
+++ queries/indexes.js
@@ -1,1 +1,12 @@
+// Index on user_id in orders collection
+// Improves performance when retrieving orders by user
+db.orders.createIndex({ user_id: 1 })
+
+// Index on product_id in reviews collection
+// Improves performance when retrieving reviews for a product
+db.reviews.createIndex({ product_id: 1 })
+
+// Index on category field in products collection
+// Improves performance when filtering products by category
+db.products.createIndex({ category: 1 })
 
