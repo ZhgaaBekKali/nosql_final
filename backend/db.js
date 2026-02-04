492c52f3d8ab058f6e54045647d7fad0f0312fa2
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 20:47:07 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
 
-const uri = "YOUR_MONGODB_CONNECTION_STRING";
+const uri = "mongodb+srv://backend_user:backend123@asik3.8hbozps.mongodb.net/online_store?retryWrites=true&w=majority&appName=asik3";
+
 const client = new MongoClient(uri);
+
+await client.connect();
 
 export const db = client.db("online_store");
