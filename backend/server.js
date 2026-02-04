0b82cd1f7fe7337c82e78f3aad731b70cc70b65a
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 14:17:22 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
+import express from "express";
+import cors from "cors";
 
+import productsRoutes from "./routes/products.js";
+import ordersRoutes from "./routes/orders.js";
+import reviewsRoutes from "./routes/reviews.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productsRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/reviews", reviewsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Online Store API is running");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
