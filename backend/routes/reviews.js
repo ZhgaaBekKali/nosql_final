e112089f4a2dc1e29d88138e2a4e4b251f8ed7f9
ZhgaaBekKali <zhalgas100307@gmail.com>
2026-02-04 14:18:11 +0500

    Update reviews.js

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { db } from "../db.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const reviews = await db.collection("reviews").find().toArray();
+  res.json(reviews);
+});
+
+export default router;
